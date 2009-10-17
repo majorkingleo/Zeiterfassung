cdf3f83f2d32d61a3c67ffa8927181d9013c8ec9
mario <mario@0809fef9-18a3-0410-bae6-c9758b01f9bc>
2009-10-17 17:51:55 +0000

    Added more PRM-check callbacks
    git-svn-id: https://svn.borger.co.at/repos/redeye/Zeiterfassung@1344 0809fef9-18a3-0410-bae6-c9758b01f9bc

--- Main/trunk/at/redeye/Zeiterfassung/AppConfigDefinitions.java
+++ Main/trunk/at/redeye/Zeiterfassung/AppConfigDefinitions.java
@@ -19,9 +19,10 @@ public class AppConfigDefinitions {
 
     public static DBConfig HoursPerWeek = new DBConfig("Wochenstunden", "38.5", "Voeingestellte Wochenstundenanzahl", new PrmDefaultCheckSuite(PrmDefaultChecksInterface.PRM_IS_DOUBLE));
     public static DBConfig NormalWorkPercent = new DBConfig("Normarbeits-Pensum", "100", "Voreingestellter Normalarbeitszeitanteil in Prozent", new PrmDefaultCheckSuite(PrmDefaultChecksInterface.PRM_IS_DOUBLE));
-    public static DBConfig NormalWorkTimeStart = new DBConfig("Normalarbeitszeitbeginn", "09:00", "Vorbelegter Wert für den Beginn der Normalarbeitszeit");
-    public static DBConfig NormalWorkTimeStop = new DBConfig("Normalarbeitszeitende", "17:00", "Vorbelegter Wert für das Ende der Normalarbeitszeit");
-    public static DBConfig DaysPerWeek = new DBConfig("Arbeitstage pro Woche", "5", "Vorbelegter Wert für die Anzahl der zu arbeintenden Tage pro Woche.", new PrmDefaultCheckSuite(PrmDefaultChecksInterface.PRM_IS_LONG));
+    public static DBConfig NormalWorkTimeStart = new DBConfig("Normalarbeitszeitbeginn", "09:00", "Vorbelegter Wert für den Beginn der Normalarbeitszeit", new PrmDefaultCheckSuite(PrmDefaultChecksInterface.PRM_IS_SHORTTIME));
+    public static DBConfig NormalWorkTimeStop = new DBConfig("Normalarbeitszeitende", "17:00", "Vorbelegter Wert für das Ende der Normalarbeitszeit", new PrmDefaultCheckSuite(PrmDefaultChecksInterface.PRM_IS_SHORTTIME));
+    private static String [] validNoOfDays = {"0", "1", "2", "3", "4", "5", "6", "7"};
+    public static DBConfig DaysPerWeek = new DBConfig("Arbeitstage pro Woche", "5", "Vorbelegter Wert für die Anzahl der zu arbeintenden Tage pro Woche.", new PrmDefaultCheckSuite(PrmDefaultChecksInterface.PRM_HAS_VALUE), validNoOfDays);
     public static DBConfig NumberOfMinimumCommentChars = new DBConfig("Minimalekommentareingabenlänge", "4", "Minimale Anzahl an Zeichen, die der Benutzer bei " +
             "einem Zeiteintrag eingeben muß. Null bedeuted das kein Kommentar eingegeben werden muß.", new PrmDefaultCheckSuite(PrmDefaultChecksInterface.PRM_IS_LONG));
     public static DBConfig AutoCreateSupProjectDefaultValue = new DBConfig("Standardwert für das Automatische anlengen von Unterprojekten", "JA",
