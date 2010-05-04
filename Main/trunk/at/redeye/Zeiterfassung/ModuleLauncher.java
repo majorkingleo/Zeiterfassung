e4cc6e4cf010dcbfa20b94304136988b819dc8d4
mario <mario@0809fef9-18a3-0410-bae6-c9758b01f9bc>
2010-05-04 20:09:50 +0000

    WizardListener implemented
    git-svn-id: https://svn.borger.co.at/repos/redeye/Zeiterfassung@1367 0809fef9-18a3-0410-bae6-c9758b01f9bc

--- Main/trunk/at/redeye/Zeiterfassung/ModuleLauncher.java
+++ Main/trunk/at/redeye/Zeiterfassung/ModuleLauncher.java
@@ -13,6 +13,7 @@ import at.redeye.FrameWork.base.LocalRoot;
 import at.redeye.FrameWork.base.prm.bindtypes.DBConfig;
 import at.redeye.FrameWork.base.prm.impl.PrmDBInit;
 import at.redeye.FrameWork.base.sequence.bindtypes.DBSequences;
+import at.redeye.FrameWork.base.wizards.impl.WizardListener;
 import at.redeye.FrameWork.utilities.StringUtils;
 import at.redeye.FrameWork.widgets.StartupWindow;
 import at.redeye.UserManagement.UserManagementDialogs;
@@ -30,155 +31,182 @@ import at.redeye.Zeiterfassung.bindtypes.DBSubProjects;
 import at.redeye.Zeiterfassung.bindtypes.DBTimeEntries;
 import at.redeye.Zeiterfassung.bindtypes.DBUserPerMonth;
 
-public class ModuleLauncher extends BaseModuleLauncher implements at.redeye.UserManagement.UserManagementListener {
-    
-    private UserManagementInterface um = null;    
-    private boolean first_run = true;    
+public class ModuleLauncher extends BaseModuleLauncher implements
+		at.redeye.UserManagement.UserManagementListener, WizardListener {
 
-    public ModuleLauncher() {
+	private UserManagementInterface um = null;
+	private boolean first_run = true;
 
-        String name = "MOMM";
+	public ModuleLauncher() {
 
-        String url = "";
-        String title;
+		String name = "MOMM";
 
-        if (at.redeye.Dongle.AppliactionModes.getAppliactionModes().isDemoVersion()) {
-            name += "-dev";
-            splash = new StartupWindow("/at/redeye/Zeiterfassung/resources/icons/redeye15b-dev.png");
-            url = "http://redeye.hoffer.cx/Zeiterfassung-developer/launch.jnlp";
-            title = "ZES-DEV";
-        } else {
-            splash = new StartupWindow("/at/redeye/Zeiterfassung/resources/icons/redeye15b.png");
-            url = "http://redeye.hoffer.cx/Zeiterfassung/launch.jnlp";
-            title = "Zeiterfassung";
-        }
+		String url = "";
+		String title;
 
-        root = new LocalRoot(name, title);
-        um = new UserDataHandling(root);
-        root.setWebStartUlr(url);
-    }
+		if (at.redeye.Dongle.AppliactionModes.getAppliactionModes()
+				.isDemoVersion()) {
+			name += "-dev";
+			splash = new StartupWindow(
+					"/at/redeye/Zeiterfassung/resources/icons/redeye15b-dev.png");
+			url = "http://redeye.hoffer.cx/Zeiterfassung-developer/launch.jnlp";
+			title = "ZES-DEV";
+		} else {
+			splash = new StartupWindow(
+					"/at/redeye/Zeiterfassung/resources/icons/redeye15b.png");
+			url = "http://redeye.hoffer.cx/Zeiterfassung/launch.jnlp";
+			title = "Zeiterfassung";
+		}
 
-    public void relogin()
-    {
-        root.closeAllWindowsNoAppExit();
-        um.setAutoLogin(false);
-        invoke();
-    }
+		root = new LocalRoot(name, title);
+		um = new UserDataHandling(root);
+		root.setWebStartUlr(url);
+	}
 
-    protected void invoke() {
+	public void relogin() {
+		root.closeAllWindowsNoAppExit();
+		um.setAutoLogin(false);
+		invoke();
+	}
 
-        CheckConfig check_config = new CheckConfig(root);
+	protected void invoke() {
 
-        setCommonLoggingLevel();
+		boolean wizardStarted = false;
 
-        boolean failed_connect_db = true;
+		CheckConfig check_config = new CheckConfig(root);
 
-        try {
-            if( root.loadDBConnectionFromSetup() )
-            {
-                failed_connect_db = false;
-            }
-        } catch (NoClassDefFoundError ex) {
-            System.out.println(ex);
-        }
+		setCommonLoggingLevel();
 
-        AppConfigDefinitions.registerDefinitions();
-        FrameWorkConfigDefinitions.registerDefinitions();
-        root.getBindtypeManager().register(new DBPb());
-        root.getBindtypeManager().register(new DBSequences());
-        root.getBindtypeManager().register(new DBTimeEntries());
-        root.getBindtypeManager().register(new DBJobType());
-        root.getBindtypeManager().register(new DBUserPerMonth());
-        root.getBindtypeManager().register(new DBConfig());
-        root.getBindtypeManager().register(new DBExtraHolidays());
-        root.getBindtypeManager().register(new DBMonthBlocks());
-        root.getBindtypeManager().register(new DBCustomers());
-        root.getBindtypeManager().register(new DBCustomerAddresses());
-        root.getBindtypeManager().register(new DBProjects());
-        root.getBindtypeManager().register(new DBSubProjects());
+		boolean failed_connect_db = true;
 
-        configureLogging();
+		try {
+			if (root.loadDBConnectionFromSetup()) {
+				failed_connect_db = false;
+			}
+		} catch (NoClassDefFoundError ex) {
+			System.out.println(ex);
+		}
 
-        if( check_config.shoudPopUpwizard() )
-        {
-            if( splash != null )
-                splash.close();
+		AppConfigDefinitions.registerDefinitions();
+		FrameWorkConfigDefinitions.registerDefinitions();
+		root.getBindtypeManager().register(new DBPb());
+		root.getBindtypeManager().register(new DBSequences());
+		root.getBindtypeManager().register(new DBTimeEntries());
+		root.getBindtypeManager().register(new DBJobType());
+		root.getBindtypeManager().register(new DBUserPerMonth());
+		root.getBindtypeManager().register(new DBConfig());
+		root.getBindtypeManager().register(new DBExtraHolidays());
+		root.getBindtypeManager().register(new DBMonthBlocks());
+		root.getBindtypeManager().register(new DBCustomers());
+		root.getBindtypeManager().register(new DBCustomerAddresses());
+		root.getBindtypeManager().register(new DBProjects());
+		root.getBindtypeManager().register(new DBSubProjects());
 
-            ConfigWizard config_wizard = new ConfigWizard(root);
+		configureLogging();
 
-            config_wizard.startWizard();
-        }
+		if (check_config.shouldPopUpWizard()) {
 
-        if( !failed_connect_db )
-        {
-            PrmDBInit prmDBInit =  new PrmDBInit(root);
-            prmDBInit.initDb();
-            
-            setLookAndFeel(root);
-        }
+			if (splash != null)
+				splash.close();
 
-        if( first_run )
-            um.addUMListener(this);
+			ConfigWizard config_wizard = new ConfigWizard(root);
 
-        if (um.tryAutoLogin() == false) {
+			config_wizard.startWizard(this);
+			wizardStarted = true;
+		}
 
-            splash.close();
-            
-            if (at.redeye.Dongle.AppliactionModes.getAppliactionModes().isDemoVersion()) {
-                um.setLogo("/at/redeye/Zeiterfassung/resources/icons/redeye15b-dev.png");
-            } else {
-                um.setLogo("/at/redeye/Zeiterfassung/resources/icons/redeye15b.png");
-            }
+		if (!failed_connect_db) {
+			PrmDBInit prmDBInit = new PrmDBInit(root);
+			prmDBInit.initDb();
 
-            um.requestDialog(UserManagementDialogs.UM_LOGIN_DIALOG);
-        } else {
-            splash.close();
-        }
+			setLookAndFeel(root);
+		}
 
-        first_run = false;
+		if (first_run)
+			um.addUMListener(this);
 
-        updateJnlp();
-    }
+		if (um.tryAutoLogin() == false) {
 
-    @Override
-    public void accessGranted() {
+			splash.close();
 
-        // Now the user is known - apply allowed value to AutoLoginUser-PRM
-        String [] autoLoginValues = {root.getLogin(), ""}; // myself or nothing
-        root.getSetup().getLocalConfig(FrameWorkConfigDefinitions.AutoLoginUser.getConfigName()).setPossibleValues(autoLoginValues);
+			if (at.redeye.Dongle.AppliactionModes.getAppliactionModes()
+					.isDemoVersion()) {
+				um
+						.setLogo("/at/redeye/Zeiterfassung/resources/icons/redeye15b-dev.png");
+			} else {
+				um
+						.setLogo("/at/redeye/Zeiterfassung/resources/icons/redeye15b.png");
+			}
+			if (wizardStarted == false) {
+				um.requestDialog(UserManagementDialogs.UM_LOGIN_DIALOG);
+			}
 
+		} else {
+			splash.close();
+		}
 
-        // Extend existing logfile-name to format log.OS-XX-APPL-XX
-        RollingFileAppender fileAppender = (RollingFileAppender) logger.getAppender(RollingFileAppender.class.getSimpleName());
+		first_run = false;
 
-        if (fileAppender != null) {
-            try {
+		updateJnlp();
+	}
 
-                String filename = fileAppender.getFile().split(".txt")[0] + "-APPL-" + root.getLogin() + ".txt";
-                System.out.println("FileName: " + filename);
-                logger.removeAppender(RollingFileAppender.class.getSimpleName());
-                RollingFileAppender rfa = new RollingFileAppender(fileAppender.getLayout(), filename);
-                logger.addAppender(rfa);
-            } catch (IOException e) {
-                JOptionPane.showMessageDialog(
-                        null,
-                        StringUtils.autoLineBreak("Der Logger konnte nicht korrekt initialisiert werden!"),
-                        "User Management", JOptionPane.WARNING_MESSAGE);
-            }
+	@Override
+	public void accessGranted() {
 
-        }
+		// Now the user is known - apply allowed value to AutoLoginUser-PRM
+		String[] autoLoginValues = { root.getLogin(), "" }; // myself or nothing
+		root.getSetup().getLocalConfig(
+				FrameWorkConfigDefinitions.AutoLoginUser.getConfigName())
+				.setPossibleValues(autoLoginValues);
 
-        checkTableVersions();
+		// Extend existing logfile-name to format log.OS-XX-APPL-XX
+		RollingFileAppender fileAppender = (RollingFileAppender) logger
+				.getAppender(RollingFileAppender.class.getSimpleName());
 
-        // Don't start as thread, because BaseDialog attempts wrong closing of
-        // application
-        new MainWin(root).setVisible(true);
+		if (fileAppender != null) {
+			try {
 
-    }
-    
-    @Override
-    public String getVersion() {
-        return Version.getVersion();
-    }
+				String filename = fileAppender.getFile().split(".txt")[0]
+						+ "-APPL-" + root.getLogin() + ".txt";
+				System.out.println("FileName: " + filename);
+				logger
+						.removeAppender(RollingFileAppender.class
+								.getSimpleName());
+				RollingFileAppender rfa = new RollingFileAppender(fileAppender
+						.getLayout(), filename);
+				logger.addAppender(rfa);
+			} catch (IOException e) {
+				JOptionPane
+						.showMessageDialog(
+								null,
+								StringUtils
+										.autoLineBreak("Der Logger konnte nicht korrekt initialisiert werden!"),
+								"User Management", JOptionPane.WARNING_MESSAGE);
+			}
+
+		}
+
+		checkTableVersions();
+
+		// Don't start as thread, because BaseDialog attempts wrong closing of
+		// application
+		new MainWin(root).setVisible(true);
+
+	}
+
+	@Override
+	public String getVersion() {
+		return Version.getVersion();
+	}
+
+	@Override
+	public void onStateChange(WizardStatus currentWizardStatus) {
+
+		if (currentWizardStatus == WizardStatus.CLOSED) {
+			System.out.println("Wizard closed!");
+			um.requestDialog(UserManagementDialogs.UM_LOGIN_DIALOG);
+		}
+
+	}
 
 }
