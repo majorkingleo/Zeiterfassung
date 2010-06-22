feffa394f11040b493cb61964fb067b60a82e88d
kingleo <kingleo@0809fef9-18a3-0410-bae6-c9758b01f9bc>
2010-06-22 20:46:59 +0000

    Korrektur des Dialoges, damis auch unter Win richtig aussieht.
    git-svn-id: https://svn.borger.co.at/repos/redeye/Zeiterfassung@1409 0809fef9-18a3-0410-bae6-c9758b01f9bc

--- Main/trunk/at/redeye/Zeiterfassung/ChangeUserName.java
+++ Main/trunk/at/redeye/Zeiterfassung/ChangeUserName.java
@@ -16,11 +16,14 @@ import at.redeye.FrameWork.base.BaseDialog;
 import at.redeye.FrameWork.base.DefaultInsertOrUpdater;
 import at.redeye.FrameWork.base.Root;
 import at.redeye.FrameWork.base.transaction.Transaction;
+import at.redeye.FrameWork.widgets.GridLayout2;
+import at.redeye.FrameWork.widgets.NoticeIfChangedTextField;
 import at.redeye.UserManagement.UserManagementInterface;
 import at.redeye.UserManagement.bindtypes.DBPb;
 import at.redeye.UserManagement.impl.AdminDlg;
 import java.util.HashMap;
 import java.util.Vector;
+import javax.swing.JLabel;
 
 /**
  *
@@ -30,11 +33,34 @@ public class ChangeUserName extends BaseDialog {
 
     DBPb pb = new DBPb();
 
+    NoticeIfChangedTextField TTitle;
+    NoticeIfChangedTextField TName;
+    NoticeIfChangedTextField TSurname;
 
     public ChangeUserName(final Root root) {
         super(root, "Benutzername");
 
         initComponents();
+
+        panel.setLayout(new GridLayout2(0,2) );
+
+        TTitle   = new NoticeIfChangedTextField();
+        TName    = new NoticeIfChangedTextField();
+        TSurname = new NoticeIfChangedTextField();
+
+
+        TTitle.setColumns(20);
+        TName.setColumns(20);
+        TSurname.setColumns(20);
+
+        panel.add(new JLabel("Titel"));
+        panel.add( TTitle );
+
+        panel.add(new JLabel("Vorname"));
+        panel.add( TName );
+
+        panel.add(new JLabel("Name"));
+        panel.add( TSurname );
 
         new AutoMBox(ChangeUserName.class.getName()) {
 
@@ -65,76 +91,54 @@ public class ChangeUserName extends BaseDialog {
             }
         };
 
-        bindVar(TTitel, pb.title);
+        bindVar(TTitle, pb.title);
         bindVar(TName, pb.name);
-        bindVar(TSurName, pb.surname);
+        bindVar(TSurname, pb.surname);
 
         var_to_gui();
+
+        setEdited(false);
     }
 
+    @Override
+    public void close()
+    {
+         AutoMBox mb = new AutoMBox(ChangeUserName.class.getName()) {
+
+            @Override
+            public void do_stuff() throws Exception {
+                gui_to_var();
+
+                Transaction trans = getTransaction();
+
+                if (pb.id.getValue() == 0) {
+                    pb.id.loadFromCopy(getNewSequenceValue(AdminDlg.UM_ID_SEQ));
+                }
+
+                DefaultInsertOrUpdater.insertOrUpdateValuesWithPrimKey(trans, pb, pb.hist, root.getUserName());
+
+                trans.commit();
+            }
+        };
+
+        if (!mb.isFailed()) {
+            super.close();
+        }
+    }
     
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
 
         jLTitle = new javax.swing.JLabel();
-        jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        TTitel = new javax.swing.JTextField();
-        TName = new javax.swing.JTextField();
-        TSurName = new javax.swing.JTextField();
         jBCancel = new javax.swing.JButton();
+        panel = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLTitle.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
         jLTitle.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLTitle.setText("Benutzername");
-
-        jLabel1.setText("Titel");
-
-        jLabel2.setText("Vorname");
-
-        jLabel3.setText("Name");
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1)
-                            .addComponent(jLabel2))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel3)
-                        .addGap(35, 35, 35)))
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(TTitel, javax.swing.GroupLayout.DEFAULT_SIZE, 304, Short.MAX_VALUE)
-                    .addComponent(TName, javax.swing.GroupLayout.DEFAULT_SIZE, 304, Short.MAX_VALUE)
-                    .addComponent(TSurName, javax.swing.GroupLayout.DEFAULT_SIZE, 304, Short.MAX_VALUE))
-                .addContainerGap())
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(TTitel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(TName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel3)
-                    .addComponent(TSurName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
 
         jBCancel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/at/redeye/FrameWork/base/resources/icons/fileclose.gif"))); // NOI18N
         jBCancel.setText("Schließen");
@@ -144,6 +148,17 @@ public class ChangeUserName extends BaseDialog {
             }
         });
 
+        javax.swing.GroupLayout panelLayout = new javax.swing.GroupLayout(panel);
+        panel.setLayout(panelLayout);
+        panelLayout.setHorizontalGroup(
+            panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 340, Short.MAX_VALUE)
+        );
+        panelLayout.setVerticalGroup(
+            panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 88, Short.MAX_VALUE)
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -151,13 +166,10 @@ public class ChangeUserName extends BaseDialog {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLTitle, javax.swing.GroupLayout.DEFAULT_SIZE, 379, Short.MAX_VALUE)
-                        .addContainerGap())
-                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jBCancel)
-                        .addContainerGap())))
+                    .addComponent(panel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLTitle, javax.swing.GroupLayout.DEFAULT_SIZE, 340, Short.MAX_VALUE)
+                    .addComponent(jBCancel, javax.swing.GroupLayout.Alignment.TRAILING))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -165,7 +177,7 @@ public class ChangeUserName extends BaseDialog {
                 .addContainerGap()
                 .addComponent(jLTitle)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(panel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jBCancel)
                 .addContainerGap())
@@ -176,41 +188,15 @@ public class ChangeUserName extends BaseDialog {
 
     private void jBCancelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBCancelActionPerformed
 
-       AutoMBox mb =  new AutoMBox(ChangeUserName.class.getName()) {
-
-            @Override
-            public void do_stuff() throws Exception {
-                gui_to_var();
-
-                Transaction trans = getTransaction();
-
-                if( pb.id.getValue() == 0 )
-                {
-                    pb.id.loadFromCopy(getNewSequenceValue(AdminDlg.UM_ID_SEQ));
-                }
-
-                DefaultInsertOrUpdater.insertOrUpdateValuesWithPrimKey(trans, pb, pb.hist, root.getUserName());
-
-                trans.commit();
-            }
-        };
-
-        if( !mb.isFailed() )
-            close();
+        close();
 
 }//GEN-LAST:event_jBCancelActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JTextField TName;
-    private javax.swing.JTextField TSurName;
-    private javax.swing.JTextField TTitel;
     private javax.swing.JButton jBCancel;
     private javax.swing.JLabel jLTitle;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel panel;
     // End of variables declaration//GEN-END:variables
 
 }
